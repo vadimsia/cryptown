84ca834b32802deaab797818768bc1fb8d4e366f
Vadim <lansibro@gmail.com>
2022-05-17 14:17:33 +0300

    region account

--- plugin/src/test/java/SolanaTest.java
+++ plugin/src/test/java/SolanaTest.java
@@ -1,7 +1,9 @@
 import com.crypteam.solana.SolanaRPC;
 import com.crypteam.solana.exceptions.AddressFormatException;
 import com.crypteam.solana.exceptions.ApiRequestException;
+import com.crypteam.solana.misc.AccountInfo;
 import com.crypteam.solana.misc.PublicKey;
+import com.crypteam.solana.misc.RegionAccountInfo;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -21,8 +23,21 @@ public class SolanaTest {
     @Test
     public void RPCTest () throws AddressFormatException, IOException {
         SolanaRPC rpc = new SolanaRPC("https://explorer-api.devnet.solana.com/");
+        PublicKey pk = new PublicKey("s5E5TCxx2DNjoENQ6wmRQNR2pVjNFC1cy1tWfaFLQNV");
+
         try {
-            rpc.getAccountInfo(new PublicKey("s5E5TCxx2DNjoENQ6wmRQNR2pVjNFC1cy1tWfaFLQNV"));
+            AccountInfo accountInfo = rpc.getAccountInfo(pk);
+            assertEquals(accountInfo.getPublicKey().toString(), pk.toString());
+        } catch (ApiRequestException e) {
+            System.out.println(e);
+        }
+
+        pk = new PublicKey("9v4t7MNLyzXRYgqwLU6bzq8jf4haWfrSziVwzXHPRDcX");
+
+        try {
+            AccountInfo accountInfo = rpc.getAccountInfo(pk);
+            RegionAccountInfo regionAccount = new RegionAccountInfo(accountInfo);
+            System.out.println(regionAccount.getOwner().toString());
         } catch (ApiRequestException e) {
             System.out.println(e);
         }

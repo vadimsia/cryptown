3e0ff571b0a573900279a8788178818fb28ef165
Vadim <lansibro@gmail.com>
2022-05-29 18:53:11 +0300

    sign

--- backend/src/test/java/com/crypteam/ApplicationTests.java
+++ backend/src/test/java/com/crypteam/ApplicationTests.java
@@ -7,8 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import software.pando.crypto.nacl.Crypto;
 
 import java.io.IOException;
-import java.nio.IntBuffer;
-import java.nio.charset.StandardCharsets;
+
 import java.security.PublicKey;
 
 @SpringBootTest
@@ -25,9 +24,9 @@ class ApplicationTests {
 
 	@Test
 	void signatureTest() {
-		int[] key = new int[] {181, 19, 83, 222, 214, 195, 190, 251, 177, 243, 14, 147, 192, 4, 198, 155, 204, 42, 63, 16, 244, 194, 64, 146, 91, 45, 187, 88, 157, 228, 137, 103};
+		byte[] key = new byte[] {68, 66, 113, 116, 82, 122, 82, 84, 121, 70, 49, 98, 106, 121, 101, 71, 76, 89, 121, 66, 69, 52, 72, 75, 80, 53, 113, 65, 81, 77, 90, 51, 50, 66, 68, 122, 68, 101, 65, 54, 117, 81, 52, 110};
 
-		PublicKey pk = Crypto.signingPublicKey("DBqtRzRTyF1bjyeGLYyBE4HKP5qAQMZ32BDzDeA6uQ4n");
+		PublicKey pk = Crypto.signingPublicKey(key);
 		byte[] sign = Base64.decodeBase64("VIMffpLOsZtEAT8UsJ8s+NW1+DMTqYbY0DM+dvdsaTYTP1/cAzdD2Ip7bex0dHXfI3oe/zEfslZEwHoyhNX9Ag==");
 		String msg = "Hello world!";
 

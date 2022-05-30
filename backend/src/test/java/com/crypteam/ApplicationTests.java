acb89ef8a9dd811a9c4d9efef325bc987eb02dce
Vadim <lansibro@gmail.com>
2022-05-30 12:54:53 +0300

    tweetnacl

--- backend/src/test/java/com/crypteam/ApplicationTests.java
+++ backend/src/test/java/com/crypteam/ApplicationTests.java
@@ -1,10 +1,10 @@
 package com.crypteam;
 
+import com.crypteam.crypto.TweetNaclFast;
 import com.crypteam.rcon.RConClient;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import software.pando.crypto.nacl.Crypto;
 
 import java.io.IOException;
 
@@ -26,10 +26,10 @@ class ApplicationTests {
 	void signatureTest() {
 		byte[] key = new byte[] {68, 66, 113, 116, 82, 122, 82, 84, 121, 70, 49, 98, 106, 121, 101, 71, 76, 89, 121, 66, 69, 52, 72, 75, 80, 53, 113, 65, 81, 77, 90, 51, 50, 66, 68, 122, 68, 101, 65, 54, 117, 81, 52, 110};
 
-		PublicKey pk = Crypto.signingPublicKey(key);
-		byte[] sign = Base64.decodeBase64("VIMffpLOsZtEAT8UsJ8s+NW1+DMTqYbY0DM+dvdsaTYTP1/cAzdD2Ip7bex0dHXfI3oe/zEfslZEwHoyhNX9Ag==");
+		TweetNaclFast.Signature sign = new TweetNaclFast.Signature(key, key);
+		byte[] signature = Base64.decodeBase64("VIMffpLOsZtEAT8UsJ8s+NW1+DMTqYbY0DM+dvdsaTYTP1/cAzdD2Ip7bex0dHXfI3oe/zEfslZEwHoyhNX9Ag==");
 		String msg = "Hello world!";
 
-		System.out.println(Crypto.signVerify(pk, msg.getBytes(), sign));
+		System.out.println(sign.detached_verify(msg.getBytes(), signature));
 	}
 }

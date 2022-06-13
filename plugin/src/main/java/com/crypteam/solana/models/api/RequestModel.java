fdd0fe8eca93e9a4c2b8089c476eeb7d1e495962
Vadim <lansibro@gmail.com>
2022-06-13 21:30:40 +0300

    fix request model

--- plugin/src/main/java/com/crypteam/solana/models/api/RequestModel.java
+++ plugin/src/main/java/com/crypteam/solana/models/api/RequestModel.java
@@ -1,8 +1,11 @@
 package com.crypteam.solana.models.api;
 
 import java.util.List;
+import java.util.UUID;
 
 public class RequestModel {
+    public String id = UUID.randomUUID().toString();
+    public String jsonrpc = "2.0";
     public String method;
 
     public List<String> params;

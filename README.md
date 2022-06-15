18806f1e63bcec9a0854afb2eb3d5454fa75f329
Vadim <lansibro@gmail.com>
2022-06-15 13:13:11 +0300

    eblomixer + gitignore

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Cryptown
 
 actual dev contract pk: u35VEZ9gPkPg1VAp3YAxPejRhKKu5q8FJagEc7vUs6Y
+actual dev candy machine pk: H82W1XdstGha16QW5SaDRcer9HzEPYWEub1KfGHhGssd
---
+++ eblomixer/eblomixer.py
@@ -0,0 +1,72 @@
+from PIL import Image
+import json
+
+from hashlib import md5
+
+class Asset:
+  image = None
+
+  id = None
+  name = None
+  dir = None
+  
+  def __init__(self, dir, name):
+    self.image = Image.open(f'{dir}/{name}').convert('RGBA')
+
+    self.dir = dir
+    self.id = name.split(' ')[0]
+    self.name = ' '.join(name.split(' ')[1:]).split('.')[0]
+
+class Art:
+  background = None
+  metadata = None
+
+  h = None
+
+  def __init__(self, width: int, height: int, index: int) -> None:
+    self.background = Image.new('RGBA', (width, height))
+
+    self.h = md5(str(index).encode()).hexdigest()
+
+    self.metadata = {
+      "name": f"Cryptown Token #{index}",
+      "symbol": "CRYPTOWN",
+      "seller_fee_basis_points": 100,
+      "image": f"{index}.png",
+      "external_url": "https://cryptown.com",
+      "attributes": [],
+      "collection": 'CRYPTOWN',
+      "properties": {
+        "files": [
+          {
+            "uri": f"{index}.png",
+            "type": "image/png"
+          }
+        ],
+        "category": "image",
+        "creators": [
+          {
+            "address": "HCMDYFaAWD3YuaBMLiftc5MzNKcLrPmjASRaciRdAAYU",
+            "share": 100
+          }
+        ]
+      }
+    }
+
+  def putAsset(self, trait_type: str, asset: Asset) -> None:
+    self.background.paste(asset.image, (0, 0), asset.image)
+
+  def save(self, dir: str, name: str):
+    self.background.save(f'{dir}/{name}.png')
+    
+    with open(f'{dir}/{name}.json', 'w') as f:
+      json.dump(self.metadata, f, indent=2)
+
+
+for i in range(0, 100):
+  art = Art(512, 512, i)
+  asset = Asset('images', 'background.png')
+
+  art.putAsset('', asset)
+
+  art.save('mixed', str(i))
